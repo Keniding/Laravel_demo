9314cd3c3193b6d6741b0a0d08fef3c196d4ad57
Henry Keniding Tarazona Lazaro <115328041+Keniding@users.noreply.github.com>
2024-08-06 17:29:24 -0500

    Uso de componentes como plantilla 2
    Uso de componentes como plantilla 2, uso de x-slot y el atributo name para hacer referencia en el componente: {{ $name: }}

--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -1,4 +1,9 @@
 <x-app-layout>
+
+    <x-slot name="title">
+        Home title laravel component
+    </x-slot>
+
     <div class="max-w-2xl mx-auto px-4">
         <h1 class="text-3xl font-bold text-gray-900 mb-4">Welcome to my page</h1>
 
