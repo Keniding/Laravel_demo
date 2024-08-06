db4e27ec7c2c7b2cbdb0f6974d98247d0a4b1bbb
Henry Keniding Tarazona Lazaro <115328041+Keniding@users.noreply.github.com>
2024-08-06 17:11:21 -0500

    Plantillas a partir de componentes
    Plantillas a partir de componentes layout

--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel 11</title>
-
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body>
+<x-app-layout>
     <div class="max-w-2xl mx-auto px-4">
         <h1 class="text-3xl font-bold text-gray-900 mb-4">Welcome to my page</h1>
 
@@ -20,6 +11,4 @@
         </x-alert>
         <p>Hello world</p>
     </div>
-
-</body>
-</html>
+</x-app-layout>
