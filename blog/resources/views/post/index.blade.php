65717685b688b20cf565f7f43afc9995e8c8f2ba
Henry Keniding Tarazona Lazaro <115328041+Keniding@users.noreply.github.com>
2024-08-06 03:23:49 -0500

    Uso de componentes anonimos
    Uso de componentes anonimos, alert y gestion de estilos tailwindcss con blade con uso de php

--- blog/resources/views/post/index.blade.php
+++ blog/resources/views/post/index.blade.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravek 11 - Post</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <h1>Post - This is the posts page</h1>
+    <div class="max-w-2xl mx-auto px-4">
+        <h1>Post - This is the posts page</h1>
+
+        <x-alert type="warning">
+            <x-slot name="title">
+                Titulo/post
+            </x-slot>
+            Contenido de la alerta variable, post
+        </x-alert>
+    </div>
 </body>
 </html>
